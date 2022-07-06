82b5366f4c44e57ede57a2d21d00275143b47b79
Maksim Abramovich <maksim_abramovich@epam.com>
2022-07-06 05:28:28 -0700

    Commit the solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,34 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	type poolToken = struct{}
+	lPoolChan := make(chan poolToken, int(pool))
+	defer close(lPoolChan)
+
+	lUsers := make([]user, 0, n)
+	var lUserLock sync.Mutex
+
+	var lWaitGroup sync.WaitGroup
+	var i int64
+	for i = 0; i < n; i++ {
+		lWaitGroup.Add(1)
+
+		go func(aId int64) {
+			defer lWaitGroup.Done()
+
+			lPoolChan <- poolToken{} // block if pool limit exceeded
+			defer func() {
+				<-lPoolChan // release pool lock
+			}()
+
+			lUser := getOne(aId)
+
+			lUserLock.Lock()
+			defer lUserLock.Unlock()
+			lUsers = append(lUsers, lUser)
+		}(i)
+	}
+	lWaitGroup.Wait()
+
+	return lUsers
 }
